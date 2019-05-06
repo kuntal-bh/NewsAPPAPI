abfac96387fa53ddc2f6503cba975ea50a39453d
kuntal <kntlbh@gmail.com>
2019-05-07 00:24:44 +0530

    commit

---
+++ NewsAppApi/Controllers/ValuesController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NewsAppApi.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "NewsApp1", "NewsApp2" };
+        }
+
+        // GET api/values/5
+       
+    }
+}
